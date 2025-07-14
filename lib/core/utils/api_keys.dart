2dc96d5202b07d7cedde919f17e1034bb5d14d4b
Sharawelly <lordmarawanshrief@gmail.com>
2025-07-14 22:47:33 +0300

    adding api keys

---
+++ lib/core/utils/api_keys.dart
@@ -0,0 +1,13 @@
+class ApiKeys {
+  static const String secretKey =
+      'sk_test_51RiyKYR8QF4pUpgJrnzPWht0p1hxRCHfKJQa8B5Ae89RC1mmWOjK47zW7ZiQuL2NY81vGWUfrYg5tEmbi1nrTw2h00uW149FNz';
+
+  static const String publishableKey =
+      'pk_test_51RiyKYR8QF4pUpgJUFNUCLYi3oblFkdujkm9uyjqx6fb8QYaaVJ8YkaeVMdVP924xOdbTuVzGFssjfE4NYycUZjy00maToYjaO';
+
+  static const String clientId =
+      'AWCmcD0aKEMGx7iIrWrEgqEss1v4n61RmtnQSmBc26SwNLZAdastyqtNJjghoQmmKwHxYx07M1GIqU8N';
+
+  static const String secretKeyPaypal =
+      'ELuO6rqolGii5IxD_i6vo2xaWO8LLfHvPIuc7wnix2WILzLKpQr1iV5VU_KQYOiT26LM1Ydkx4pkECFc';
+}
